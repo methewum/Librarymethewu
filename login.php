51f99b006a010f3424c7e1b95410a16aa1b0b132
methewum <methewum@gmail.com>
2024-11-10 23:57:21 +0700

    Update login.php
    Signed-off-by: methewum <methewum@gmail.com>

--- login.php
+++ login.php
@@ -1,61 +1,35 @@
-<?php
-session_start();
-?>
-<!doctype html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
-	<title>Login Admin jualan</title>
+<title>Login Anggota</title>
+<link rel="stylesheet" type="text/css" href="css/Login.css">
 </head>
 <body>
-	<div class="container">
-			
+<form method="post" action="login_admin.php">
 
-		<div class="row">
-			<div class="col-md-4 offset-md-4 mt-5">
-				
+<div class="tampilan">
+ <div class="kepala">
+<div class="logo"></div>
+<h2 class="judul">Login Anggota</h2>
+</div>
+<div class="artikel">
 
-				<?php
-				if(isset($_SESSION['error'])) {
-				?>
-				<div class="alert alert-warning" role="alert">
-				  <?php echo $_SESSION['error']?>
-				</div>
-				<?php
-				}
-				?>
-				
-				<div class="card ">
-					<div class="card-title text-center">
-						<h1>Login Admin</h1>
-					</div>
-					<div class="card-body">
-						<form action="process.php" method="post">
-							<div class="form-group">
-								<label for="username">Nama Admin</label>
-								<input type="text" name="username" class="form-control" id="username" aria-describedby="username" placeholder="Nama Admin">
-							</div>
-							<div class="form-group">
-								<label for="password">Password</label>
-								<input type="password" name="password" class="form-control" id="password" placeholder="Password">
-							</div>
-							<button type="submit" class="btn btn-primary">Masuk</button>
-						</form>
-					</div>
-				</div>
-				<br>
-				<ul class="nav">
-					<li class="nav-item">
-					<a href="index.hml" class="btn btn-sm btn-primary float-right"><=Kembali</a>
-					</li>
-				</ul>
-				<br>
-			</div>
+<div class="kesalahan">
+<?php
+        if(isset($_GET["login_error"])){
+            echo "Username atau password salah";
+        }
+    ?>
+</div>
 
-		</div>
+<div class="kotak">
+<p><input type="text" name="username" value="" placeholder="Masukan Username Anda"></p>
+<p><input type="password" name="password" value="" placeholder="Masukan Password Anda"></p>
+<p class="submit"><input type="submit" name="commit" value="Login"></p>
+</form>
+</div>
+</div>
 
-	</div>
+ </div>
+</div>
 </body>
+</html> 
