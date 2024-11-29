881cba7eb4f981ac8b1ce2e3abf1a3ce4dc81bb0
methewum <methewum@gmail.com>
2024-11-29 15:21:55 +0700

    Update login_admin.php
    Signed-off-by: methewum <methewum@gmail.com>

--- login_admin.php
+++ login_admin.php
@@ -1,17 +1,4 @@
 <?php
     
-    ob_start();
-    session_start();
-    ob_end_clean();
-    
-    $username=$_POST["username"];
-    $password=$_POST["password"];
-    
-    if($username=="admin" AND $password=="admin")
-    {
-        $_SESSION["username"]=$username;
-        header("location:home.php");
-    }else{
-        header("location:login.php?login_error");
-    }
+    <script> function validateForm() { var username = document.getElementById('username').value; var password = document.getElementById('password').value; if (username == "admin" && password == "12345") { window.location.href = "menuutama.html"; } else { alert("Username atau Password salah!"); return false; } } </script>
 ?>
