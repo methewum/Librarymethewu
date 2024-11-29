80f27cf10d643024beed88f4ce3ad7e83e3f6257
methewum <methewum@gmail.com>
2024-11-29 17:47:59 +0700

    Update login.php
    Signed-off-by: methewum <methewum@gmail.com>

--- login.php
+++ login.php
@@ -1,27 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Form Login</title>
+<title>Login </title>
 </head>
 <body>
-    <form action="login.php" method="POST">
-        <fieldset>
-        <legend>Login</legend>
-        <p>
-            <label>Username:</label>
-            <input type="text" name="username" placeholder="username..." />
-        </p>
-        <p>
-            <label>Password:</label>
-            <input type="password" name="password" placeholder="password..." />
-        </p>
-        <p>
-            <label><input type="checkbox" name="remember" value="remember" /> Remember me</label>
-        </p>
-        <p>
-            <input type="submit" name="submit" value="Login" />
-        </p>
-        </fieldset>
-    </form>
+<form method="POST" action="login.php">
+Username: <input type="text" name="username" size="20">
+Password: <input type="password" name="password" size="20">
+<input type="submit" value="Login" name="login">
+</form>
 </body>
 </html>
